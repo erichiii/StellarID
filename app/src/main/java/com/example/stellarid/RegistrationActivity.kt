b6a4596b7409604424c46a2445c6ad77e03860ac
erichiii <angel.letada1205@gmail.com>
2025-10-30 01:07:49 +0800

    added button onClickListener

--- app/src/main/java/com/example/stellarid/RegistrationActivity.kt
+++ app/src/main/java/com/example/stellarid/RegistrationActivity.kt
@@ -1,29 +1,20 @@
 package com.example.stellarid
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val getStartedButton = findViewById<ImageButton>(R.id.getStartedButton)
-
-        getStartedButton.setOnClickListener {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            startActivity(intent)
         }
     }
 }
