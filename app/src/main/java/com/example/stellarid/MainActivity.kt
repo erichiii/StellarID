a1b85d286de66a103e08811ad302c09e8dbe480e
erichiii <angel.letada1205@gmail.com>
2025-10-30 22:52:35 +0800

    added an exit dialog for seamless UX

--- app/src/main/java/com/example/stellarid/MainActivity.kt
+++ app/src/main/java/com/example/stellarid/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.stellarid
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -25,5 +27,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle("Exit Application")
+                    .setMessage("Are you sure you want to exit?")
+                    .setPositiveButton("Exit") { _, _ ->
+                        finish()
+                    }
+                    .setNegativeButton("Cancel", null)
+                    .show()
+            }
+        })
     }
 }
